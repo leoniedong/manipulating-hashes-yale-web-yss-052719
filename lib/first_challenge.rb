bfcdc781df38990d835ddbcf68775ba3babfa799
Leonie Dong <leoniedong@hotmail.com>
2019-04-11 19:41:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,8 @@ def first_challenge
 contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
 contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
 contacts.each do |person, data|
-  puts "#{person}: #{data}"
+  data.each do |attribute, value|
+    puts "#{attribute}: #{value}"
 end
 
   #remember to return your newly altered contacts hash!
